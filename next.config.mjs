8969d34588735c0697a847524e23f2e1e540edef
znt7cwdygc-crypto <znt7cwdygc@privaterelay.appleid.com>
2026-07-05 11:08:51 +0300

    Add Yandex Metrika counter (110404557) with webvisor/clickmap, expand CSP

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,8 @@ const csp = [
   "object-src 'none'",
   "img-src 'self' data: blob: https:",
   "style-src 'self' 'unsafe-inline'",
-  `script-src 'self' 'unsafe-inline' https://www.googletagmanager.com${process.env.NODE_ENV === "development" ? " 'unsafe-eval'" : ""}`,
-  `connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://analytics.google.com${devConnectSrc}`,
+  `script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://mc.yandex.ru${process.env.NODE_ENV === "development" ? " 'unsafe-eval'" : ""}`,
+  `connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://analytics.google.com https://mc.yandex.ru${devConnectSrc}`,
   "frame-src https://www.googletagmanager.com",
   "font-src 'self' data:"
 ].join("; ");
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Script id="gtm-init" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5JBDX2SG');`}
         </Script>
+        <Script id="ym-init" strategy="afterInteractive">
+          {`(function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110404557', 'ym');
+          ym(110404557, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`}
+        </Script>
       </head>
       <body>
         <noscript>
@@ -56,6 +65,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           />
+        </noscript>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/110404557" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
         </noscript>
         <DesignSystemStyles />
         <Header />
